3c1472e085b3a6a22424e777918972c6d4ae6f83
jinsb <jinsb1999@naver.com>
2023-02-15 17:14:37 +0900

    feat: 기본 레이아웃 및 스프레드시트 연결

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import RegisterPage from './pages/RegisterPage/RegisterPage.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <RegisterPage/>
     </div>
   );
 }
