280dae55283a37e203c558b3cb2b4e10190faaca
jinsb <jinsb1999@naver.com>
2023-02-22 10:34:39 +0900

    feat: Footer 컴포넌트 추가

---
+++ src/component/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterBackground  = styled.div`
+    width : 100%;
+    height : 150px;
+    background-color: #4D2EDD;
+`
+
+const Footer = () => {
+    return (
+            <FooterBackground/>
+    );
+};
+
+export default Footer;
