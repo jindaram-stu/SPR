24d38b858d10601a9c0499be3c72baab96734515
jinsb <jinsb1999@naver.com>
2023-02-22 10:40:24 +0900

    feat: 신청 마감 여부 hook 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { useState } from 'react';
+import axios from 'axios';
+import RegisterLimitPage from 'pages/RegisterLimitPage/RegisterLimitPage';
+import { useEffect, useState } from 'react';
 import RegisterDonePage from './pages/RegisterDonePage/RegisterDonePage';
 import RegisterPage from './pages/RegisterPage/RegisterPage.js'
 
 function App() {
-
+  
+  const [canRegister, setCanRegister] = useState(true);
+  const [loaded, setLoaded] = useState(false);
   const [registerDone, setRegisterDone] = useState(false);
+
+  useEffect( () => {
+    axios.get("https://script.google.com/macros/s/AKfycbwZMUMfolRTm8bHUBVYVDYuO7cevAuYpGAJjLpT_7Rfi5xvt98CkLbWnLekLG8B3mt_HQ/exec?canRegister=true")
+    .then((res)=> {
+      console.log(res.data.canRegister);
+      setCanRegister(res.data.canRegister);
+      setLoaded(true);
+    })
+  },[])
 
   return (
     <div className="App">
-      { registerDone ? <RegisterDonePage/> : <RegisterPage setRegisterDone={setRegisterDone}/>}
+      { loaded ? 
+          canRegister ? registerDone ? <RegisterDonePage/> : <RegisterPage setRegisterDone={setRegisterDone}/> : <RegisterLimitPage/>
+      : <></> }
     </div>
   );
 }
