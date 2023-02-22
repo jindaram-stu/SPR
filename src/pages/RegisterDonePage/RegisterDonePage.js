cf82048dcd4855b8ca194512bea0a28ff6d722e4
jinsb <jinsb1999@naver.com>
2023-02-22 10:39:35 +0900

    refactor: 신청 완료 페이지 styled-component 적용

--- src/pages/RegisterDonePage/RegisterDonePage.js
+++ src/pages/RegisterDonePage/RegisterDonePage.js
@@ -2,7 +2,19 @@ import React from 'react';
 import CheckSrc from 'resource/check.png'
 import styled from 'styled-components';
 
-const Center = styled.div`
+const RegisterDonePage = () => {
+    return (
+        <CenterContainer>
+            <Container>
+                <Check src={CheckSrc}/>
+                <RegisterDoneMessage>사전예약이 완료되었습니다.</RegisterDoneMessage>
+                <SubDescription>많은 분들께 사랑받는 <span style={{color : "#6698e8"}}>세이플리</span>가 되겠습니다.</SubDescription>
+            </Container>
+        </CenterContainer>
+    );
+};
+
+const CenterContainer = styled.div`
     width : 100%;
     height : 100vh;
     display : flex;
@@ -25,15 +37,9 @@ const RegisterDoneMessage = styled.h1`
     font-weight : bold;
 `
 
-const RegisterDonePage = () => {
-    return (
-        <Center>
-            <Container>
-                <Check src={CheckSrc}/>
-                <RegisterDoneMessage>사전등록이 완료되었습니다.</RegisterDoneMessage>
-            </Container>
-        </Center>
-    );
-};
+const SubDescription = styled.span`
+    color : gray;
+    font-size : 13px;
+`;
 
 export default RegisterDonePage;
